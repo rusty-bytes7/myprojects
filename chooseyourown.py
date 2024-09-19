421406a46f23404979cf0baef846128145c670f4
rusty-bytes7 <147008821+rusty-bytes7@users.noreply.github.com>
2024-09-19 13:03:14 -0400

    ᓚ₍ ^. .^₎

--- chooseyourown.py
+++ chooseyourown.py
@@ -1,16 +1,21 @@
-#this is a choose your own adventure game
-from time import sleep
+#this is a choose your own adventure game about Rusty the cat!
 
+#import stuff
+from time import sleep
+import random
+
+
+#start the game
 def intro():
     print("Hello player! Please input your name.")
     name = input()
     print(f"Good to meet you, {name}!")
     print("\nLet's get started.")
     sleep(3)
-    print("\nYou are a black long-haired cat named Rusty. You live a quiet, cushy life at your home with your family.")
+    print("\nYou are a black long-haired cat named Rusty. \nYou have bright green eyes and cute, crinkly ears. \nYou live a quiet, cushy life at your home with your family.")
     print("One day, your human goes to work and you notice the front door is left ajar.")
     print("\nDo you:\n  A. Stay put - outside is scary!\n  B. Wander over to inspect.")
-    choose_door() #this is a test to see if i can do git
+    choose_door()
 
 #choose to go outside or stay inside- if you stay inside the game ends
 #going outside takes you to the path
@@ -71,6 +76,7 @@ def go_left():
             print("\nYou start to wonder if you'll ever make it out of this strange tunnnel, when suddenly you see a faint light in the distance.")
             print("\nYou pick up the pace, and the light grows brighter and warmer before you.")
             print("\nYou follow the light until you reach a cavernous room, and realize you're in a gigantic library.")
+            library()
             # Continue the story here or add another function
             break
         
@@ -84,6 +90,10 @@ def go_left():
         else:
             print("Rusty doesn't understand! Please choose 'A' or 'B'.")
 
+#rusty finds himself in a cavernous library- happens after going through doorway of graffiti wall
+def library():
+    pass #add more story here
+
 #player chooses go right or does not enter door in graffiti wall- weird tree with runes
 def go_right():
 #maybe make the printed text easier to read
@@ -94,7 +104,7 @@ def go_right():
     print("\nYou're just about to step into the tunnel, when all of a sudden you're lifted by the scruff off the ground by sharp claws, accompanied by the sound of wings beating.")
     print("\nThe raven squawks at you, 'Nobody may enter! Pass the test, pass the test!', as he flies high into the air.")
     print("----------")
-    print("\nYou are lifted high above the city- the raven flies you to a clearing in some nearby woods. You get dropped into the clearing, and the raven alights on a branch. The air feels thick, and you feel uneasy.")
+    print("\nThe raven flies you to a clearing in some nearby woods. You get dropped into the clearing, and the raven alights on a branch. The air feels thick, and you feel uneasy.")
     print("\nThe raven chirps 'Something hunts you, but not all dangers are real.'")
     print("\nThe clearing starts to grow dark. You see the outline of a large, menacing shape moving in the trees. The creature’s growl sends shivers down your spine. \nIt's bigger than any dog you've ever seen. It circles closer, but remains hidden in the shadows.")
 
@@ -106,25 +116,29 @@ def go_right():
         answermonster = input().lower()
         if answermonster == "a":
             standground()
+            print("----------")
+            #stands ground and gets led back to tree
+            print("\nThe raven leads you back to the base of the tree with the strange runes. He allows you to enter the tunnel.")
+            print("\n'Good luck brave one,' the raven squawks.")
+            print("\nYou step into the tunnel. It's very dark, and it smells like fresh earth.")
+            tunnel()
             break
+        
         elif answermonster== "b":
         #player basically has to choose a to face the monster, otherwise will loop around
             runfrom_monster()
         else:
             print("Rusty doesn't understand! Please choose 'A' or 'B'.")
-    # You can add more choices here, similar to the left path
-    pass  # Placeholder for further story development
 
 #standing ground in face of monster
 def standground():
     print("----------")
-    print("\nYou see the creature's glowing red eyes in the trees. They start to come closer.")
+    print("\nYou see the creature's glowing red eyes in the trees.")
     print("\nThe hair on your back stands up, but something tells you to stay put.")
-    print("\nYou unsheath your claws as the ground rumbles beneath you- the creature is running toward you with astonishing speed.")
     print("\nYou let out a hiss as the dark entity speeds directly at you- and then vanishes in a puff of smoke and hot air.")
     print("\nThe raven lands on the ground in front of you.")
     print("\n'*squawk* Very good, very brave!' he says.")
-    print("\n'You passed the test!' ")
+    print("\n'You passed the test!'")
     print ("\n \n")
 
 #running from monster- will not go anywhere, player has to fight monster
@@ -136,6 +150,30 @@ def runfrom_monster():
     print("----------")
     print("\nWhat do you do?")
     print("\nA. Stand your ground. \nB. Run.")
+
+#rusty has passed the test of bravery and is allowed to enter the tunnel
+def tunnel():
+    print("----------")
+    print("\nAs you begin your trek through the dark tunnel, you begin to see faint symbols on the walls.")
+    print("\nYou realize the walls have the same faintly glowing runes as the tree.")
+    print("\nThe light grows dimmer the farther you walk, and it begins to feel as though you've been pulled into another realm entirely.")
+    print("\nSuddenly, the ground begins to shake. The floor underneath gives way, and to your surprise, you slide down into a hidden underground chamber.")
+    print("----------")
+    print("\nAs you shake off the dust from your fur, you take note and see the underground cavern is filled with glowing muchrooms and crystals that illuminate the room.")
+    print("\nThere is a strange shimmering portal on the far wall.")
+    print("\nYou try to go over and inspect, but suddenly, the shadows start to swirl and change shape.")
+    print("\nYou realize you may have to fight your way to the portal.")
+    
+    #create monster to fight
+    shadowmonster = weakmonster()
+    shadowmonster.name = "Shadow Monster"
+    print(shadowmonster.strength)
+
+#create class of monsters
+class weak_monster:
+    name = ""
+    strength = random.randint(1,10)
+
 
 def main():
     intro()
